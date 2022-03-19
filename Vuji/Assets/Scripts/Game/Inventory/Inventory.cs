d04647764703ecff9e720d5885d3b4ba156f4960
Левкин Артем <artlevkin@gmail.com>
2022-03-19 13:17:29 +0300

    Базовая панель инвентаря

--- Vuji/Assets/Scripts/Game/Inventory/Inventory.cs
+++ Vuji/Assets/Scripts/Game/Inventory/Inventory.cs
@@ -1,13 +1,24 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
-    [SerializeField] List<BaseItem> inventoryItems = new List<BaseItem>();
+    [SerializeField] BaseItem to_add;
+    public Action<BaseItem> onItemAdded;
+
+    [SerializeField] public List<BaseItem> inventoryItems = new List<BaseItem>();
+
+    public void Start()
+    {
+        AddItem(to_add);
+        AddItem(to_add);
+    }
 
     public void AddItem(BaseItem item){
         inventoryItems.Add(item);
+        onItemAdded?.Invoke(item);
         Debug.Log("Added item: " + item.GetItemName() + item.GetDescription() +  item.GetAmount());
     }
 
---
+++ Vuji/Assets/Scripts/Game/Inventory/InventoryWindow.cs
@@ -0,0 +1,116 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryWindow : MonoBehaviour
+{
+    [SerializeField] Image highlight;
+    [SerializeField] Inventory playerInventory;
+    [SerializeField] RectTransform inventoryPanel;
+    private int currentItemIndex = 0;
+
+    List<GameObject> displayedIcons = new List<GameObject>();
+
+    public void Start()
+    {
+        playerInventory.onItemAdded += OnItemAdded;
+        Redraw();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            BaseItem currentItem = playerInventory.inventoryItems[currentItemIndex];
+            bool dropAll = Input.GetKeyDown(KeyCode.LeftControl);
+            bool hasItem = currentItem.DropItem(dropAll);
+            if (!hasItem)
+            {
+                playerInventory.inventoryItems.Remove(currentItem);
+                currentItemIndex--;
+                if (currentItemIndex < 0)
+                {
+                    currentItemIndex = 0;
+                }
+            }
+            Redraw();
+        }
+
+
+        if (displayedIcons.Count < 1) return;
+
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            currentItemIndex = (currentItemIndex + 1);
+            if (currentItemIndex > playerInventory.inventoryItems.Count - 1)
+            {
+                currentItemIndex = 0;
+            }
+            else if (currentItemIndex < 0)
+            {
+                currentItemIndex = playerInventory.inventoryItems.Count - 1;
+            }
+            Debug.Log(currentItemIndex.ToString());
+            Redraw();
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            currentItemIndex = (currentItemIndex - 1);
+            if (currentItemIndex > playerInventory.inventoryItems.Count - 1)
+            {
+                currentItemIndex = 0;
+            }
+            else if (currentItemIndex < 0)
+            {
+                currentItemIndex = playerInventory.inventoryItems.Count - 1;
+            }
+            Debug.Log(currentItemIndex.ToString());
+            Redraw();
+        }
+    }
+
+
+    void OnItemAdded(BaseItem item) => Redraw();
+
+    void Redraw()
+    {
+        highlight.transform.SetParent(inventoryPanel);
+        highlight.gameObject.SetActive(false);
+        ClearDisplayedItems();
+        for (var i = 0; i < playerInventory.inventoryItems.Count; i++)
+        {
+            var item = playerInventory.inventoryItems[i];
+            
+            if (item != null)
+            {
+                var icon = new GameObject(name: "Icon");
+                icon.AddComponent<Image>().sprite = item.GetImage();
+                icon.transform.SetParent(inventoryPanel);
+                if (i == currentItemIndex)
+                {
+                    if (!highlight.gameObject.activeSelf)
+                    {
+                        highlight.gameObject.SetActive(true);
+                    }
+                    highlight.transform.SetParent(icon.transform);
+                    
+                    highlight.transform.position = icon.transform.position;
+                }
+                displayedIcons.Add(icon);
+            }
+        }
+    }
+
+    void ClearDisplayedItems()
+    {
+        for (var i = 0;i < displayedIcons.Count; i++)
+        {
+            Destroy(displayedIcons[i]);
+        }
+        displayedIcons.Clear();
+    }
+}
