2e90d20c4ae9a9c7469a715743f9779784ea66d8
Ivan Mashanov <imashanov06@gmail.com>
2022-04-03 19:42:29 +0300

    сецена с ареной

--- Vuji/Assets/Scripts/Lobby/LobbyManager.cs
+++ Vuji/Assets/Scripts/Lobby/LobbyManager.cs
@@ -59,7 +59,7 @@ public class LobbyManager : MonoBehaviourPunCallbacks
 
     public override void OnJoinedRoom()
     {
-        PhotonNetwork.LoadLevel("Game");
+        PhotonNetwork.LoadLevel("GenerateTest");
     }
 
     #endregion
